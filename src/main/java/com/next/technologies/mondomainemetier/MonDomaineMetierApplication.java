0f0ebbd824fe7ec206fa53c94c6388be8de8a647
Namory Traore <namorytraore1984@yahoo.fr>
2021-12-22 17:19:26 -0500

    Premier commit

---
+++ src/main/java/com/next/technologies/mondomainemetier/MonDomaineMetierApplication.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomainemetier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonDomaineMetierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonDomaineMetierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/next/technologies/mondomainemetier/MonDomaineMetierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomainemetier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonDomaineMetierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
